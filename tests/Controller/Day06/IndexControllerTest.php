93542cf8c87db65497da76a2d4a3c2da47f7074d
Miguel Ángel <migueabellan@gmail.com>
2020-12-06 12:06:19 +0100

    Add Day 6

---
+++ tests/Controller/Day06/IndexControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Controller\Day06;
+
+use App\Controller\Day06\IndexController;
+use PHPUnit\Framework\TestCase;
+
+class IndexControllerTest extends TestCase
+{
+    /**
+     * @var object
+     */
+    private object $runner;
+
+    /**
+     * @var array
+     */
+    private array $array;
+
+    protected function setUp(): void
+    {
+        $this->runner = new IndexController();
+
+        $this->array = $this->runner->read();
+    }
+
+    public function testExec1(): void
+    {
+        $this->assertEquals(6273, $this->runner->exec1($this->array));
+    }
+
+    public function testExec2(): void
+    {
+        $this->assertEquals(3254, $this->runner->exec2($this->array));
+    }
+}
