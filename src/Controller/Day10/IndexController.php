305bbd024ba6c20511bd68eb68174f9781ace43e
Miguel Ángel <migueabellan@gmail.com>
2020-12-10 20:59:37 +0100

    Refactor 10

---
+++ src/Controller/Day10/IndexController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller\Day10;
+
+use App\Controller\AbstractController;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @see AbstractController
+     */
+    public function read(): array
+    {
+        $array = [];
+
+        if ($file = fopen($this->getPathIn(), 'r')) {
+            while (($line = fgets($file)) !== false) {
+                $array[] = (int)$line;
+            }
+            fclose($file);
+        }
+
+        $array[] = 0;
+        $array[] = max($array) + 3;
+
+        sort($array);
+        
+        return $array;
+    }
+
+    public function exec1(array $array = []): string
+    {
+        $diff = [];
+        for ($i = 0; $i < (count($array) - 1); $i += 1) {
+            $diff[] = $array[$i + 1] - $array[$i];
+        }
+
+        $group = array_count_values($diff);
+
+        return (string)($group[1] * $group[3]);
+    }
+
+    public function exec2(array $array = []): string
+    {
+        $result = 0;
+
+        $result = [1];
+        for ($i = 1; $i < count($array); $i++) {
+            $aux = 0;
+            for ($j = max(0, $i - 3); $j < $i; $j++) {
+                if (($array[$i] - $array[$j]) <= 3) {
+                    $aux += $result[$j];
+                }
+            }
+            $result[] = $aux;
+        }
+
+        return (string)end($result);
+    }
+}
