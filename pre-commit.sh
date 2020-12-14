f17b9e7bb9e28031f764d52409bbd7c921cc2ceb
Miguel Ángel <migueabellan@gmail.com>
2020-12-14 19:08:45 +0100

    Add parallel

--- pre-commit.sh
+++ pre-commit.sh
@@ -3,4 +3,5 @@
 docker run -it --rm -v "$PWD":/app composer cscheck
 docker run -it --rm -v "$PWD":/app composer csfix
 docker run -it --rm -v "$PWD":/app composer phpstan
+docker run -it --rm -v "$PWD":/app composer parallel
 docker run -it --rm --name aoc -v "$PWD":/app -w /app php:7.4-cli php vendor/bin/phpunit --testdox tests
