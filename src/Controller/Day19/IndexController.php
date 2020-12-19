bb51a155413f93accb1ee07e43cfed53def081ff
Miguel Ángel <migueabellan@gmail.com>
2020-12-19 12:18:23 +0100

    Add puzzle 1

---
+++ src/Controller/Day19/IndexController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller\Day19;
+
+use App\Controller\AbstractController;
+
+class IndexController extends AbstractController
+{
+    private array $messages = [];
+
+    /**
+     * @see AbstractController
+     */
+    public function read(): array
+    {
+        $array = [];
+
+        if ($file = fopen($this->getPathIn(), 'r')) {
+            while (($line = fgets($file)) !== false) {
+                $line = trim($line);
+
+                if (preg_match("~^(?'k'.*): (?'expr'.*)$~", $line, $matches)) {
+                    $expr = $matches['expr'];
+                    if (preg_match("~^(?'first'.*) | (?'second'.*)$~", $matches['expr'], $submatches)) {
+                        $expr = $matches['expr'];
+                    }
+
+                    $array[$matches['k']] = explode(' ', str_replace('"', '', $expr));
+                }
+
+                if (preg_match('/^[a-z]+$/', $line)) {
+                    $this->messages[] = $line;
+                }
+            }
+            fclose($file);
+        }
+
+        return $array;
+    }
+
+    private function setRegex(array $array, int $rule_id): string
+    {
+        $current = current($array[$rule_id]);
+        if ($current === 'a' || $current === 'b') {
+            return $current;
+        }
+
+        $expr = '(';
+        foreach ($array[$rule_id] as $v) {
+            if ($v !== '|') {
+                $expr .= $this->setRegex($array, $v);
+            } else {
+                $expr .= '|';
+            }
+        }
+        $expr .= ')';
+
+        return $expr;
+    }
+
+    public function exec1(array $array = []): string
+    {
+        $result = 0;
+
+        $regex = $this->setRegex($array, 0);
+
+        foreach ($this->messages as $message) {
+            if (preg_match('/^'.$regex.'$/', $message)) {
+                $result++;
+            }
+        }
+
+        return (string)$result;
+    }
+    
+
+    public function exec2(array $array = []): string
+    {
+        $result = 0;
+
+        return (string)$result;
+    }
+}
---
+++ tests/Controller/Day19/IndexControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Controller\Day19;
+
+use App\Controller\Day19\IndexController;
+use PHPUnit\Framework\TestCase;
+
+class IndexControllerTest extends TestCase
+{
+    /**
+     * @var IndexController
+     */
+    private object $runner;
+
+    /**
+     * @var array
+     */
+    private array $array;
+
+    protected function setUp(): void
+    {
+        $this->runner = new IndexController();
+
+        $this->array = $this->runner->read();
+    }
+
+    public function testExec1(): void
+    {
+        $this->assertEquals(233, $this->runner->exec1($this->array));
+    }
+
+    public function testExec2(): void
+    {
+        $this->assertEquals(0, $this->runner->exec2($this->array));
+    }
+}
