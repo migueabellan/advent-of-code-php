a87dcc6e6dfe624b673ffe57cfced9d7273f1c4b
Miguel Ángel <migueabellan@gmail.com>
2021-01-02 10:45:31 +0100

    Update

---
+++ src/Year2015/Day03/IndexController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Year2015\Day03;
+
+use App\Controller\AbstractController;
+
+class IndexController extends AbstractController
+{
+    public function read(): array
+    {
+        $array = [];
+
+        if ($file = fopen($this->getPathIn(), 'r')) {
+            while (($line = fgets($file)) !== false) {
+                preg_match("~^(?'l'.*)x(?'w'.*)x(?'h'.*)$~", $line, $matches);
+                $l = (int)$matches['l'];
+                $w = (int)$matches['w'];
+                $h = (int)$matches['h'];
+
+                $array[] = [
+                    'l' => $l,
+                    'w' => $w,
+                    'h' => $h
+                ];
+            }
+            fclose($file);
+        }
+
+        return $array;
+    }
+
+    public function exec1(array $array = []): string
+    {
+        $result = 0;
+
+        foreach ($array as $present) {
+            $areas = [
+                $present['l'] * $present['w'],
+                $present['w'] * $present['h'],
+                $present['h'] * $present['l']
+            ];
+
+            $surface = array_reduce($areas, function ($surface, $area) {
+                return $surface += (2 * $area);
+            }, 0);
+
+            $result += ($surface + min($areas));
+        }
+        
+        return (string)$result;
+    }
+
+    public function exec2(array $array = []): string
+    {
+        $result = 0;
+
+        foreach ($array as $present) {
+            $perimeter = [
+                2 * $present['l'],
+                2 * $present['w'],
+                2 * $present['h']
+            ];
+            sort($perimeter);
+            $perimeter = $perimeter[0] + $perimeter[1];
+
+            $volume = $present['l'] * $present['w'] * $present['h'];
+
+            $result += ($perimeter + $volume);
+        }
+        
+        return (string)$result;
+    }
+}
