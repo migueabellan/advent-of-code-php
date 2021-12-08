efeb8e99e9680b33a86ab3542ad4c07d2b59ca3a
Miguel Ángel <migueabellan@gmail.com>
2021-12-08 12:06:47 +0100

    feat: update pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,4 +4,4 @@ docker-compose exec php composer cscheck
 docker-compose exec php composer csfix
 docker-compose exec php composer phpstan
 docker-compose exec php composer parallel
-docker-compose exec php vendor/bin/phpunit --testdox tests/Utils
+# docker-compose exec php vendor/bin/phpunit --testdox tests/Year2021
